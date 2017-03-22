cd82e1e115528ad88a242112726624ffad47f938
Daniel Castro García <dandev237@gmail.com>
2017-03-22 20:18:09 +0100

    Level sketched out, basic 3rd person camera implemented
    All standard assets imported (TODO: Reduce assets by keeping only Ethan's dependencies).

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private const string PlayerTag = "Player";
+    private GameObject player;
+
+    private void Start()
+    {
+        //Locate player object by tag
+        player = GameObject.FindGameObjectWithTag(PlayerTag);
+        print(PlayerTag + " found!!");
+    }
+
+    //Called in LateUpdate because the player is moved inside Update
+    private void LateUpdate()
+    {
+        this.transform.position = player.transform.position;
+    }
+}
