179d06419141a9e2552d50f2f629ae37d361c0c3
Daniel Castro García <dandev237@gmail.com>
2017-03-25 00:07:38 +0100

    HUD First iteration completed

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float maxHealthPoints = 100.0f;
+    private float currentHealthPoints;
+
+    private void Start()
+    {
+        currentHealthPoints = maxHealthPoints;
+    }
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoints / (float)maxHealthPoints;
+        }
+    }
+}
---
+++ Assets/Enemies/EnemyUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+// Add a UI Socket transform to your enemy
+// Attach this script to the socket
+// Link to a canvas prefab that contains enemy UI
+public class EnemyUI : MonoBehaviour
+{
+    // Works around Unity 5.5's lack of nested prefabs
+    [Tooltip("The UI canvas prefab")]
+    [SerializeField]
+    private GameObject enemyCanvasPrefab = null;
+
+    private Camera cameraToLookAt;
+
+    private void Start()
+    {
+        cameraToLookAt = Camera.main;
+        Instantiate(enemyCanvasPrefab, transform.position, Quaternion.identity, transform);
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(cameraToLookAt.transform);
+        transform.rotation = Quaternion.LookRotation(cameraToLookAt.transform.forward);
+    }
+}
