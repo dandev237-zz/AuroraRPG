1d6492ae926ad4655d5c9f0d1986c8a981c09cfd
Daniel Castro García <dandev237@gmail.com>
2017-04-04 15:06:21 +0200

    Added editor tools (SelectAllOfTags, SpriteProcessor)

---
+++ Assets/Editor/SelectAllOfTag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SelectAllOfTag : ScriptableWizard
+{
+	const string WizardName = "Select All Of Tag...";
+	const string CreateButtonName = "Make Selection";
+	public string searchTag = string.Empty;
+
+	[MenuItem ("My Tools/" + WizardName)]
+	static void SelectAllOfTagWizard()
+	{
+		ScriptableWizard.DisplayWizard<SelectAllOfTag>(WizardName, CreateButtonName);
+	}
+
+	void OnWizardCreate()
+	{
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(searchTag);
+		Selection.objects = gameObjects;	//Select all found gameobjects on hierarchy
+	}
+
+}
---
+++ Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SpriteProcessor : AssetPostprocessor
+{
+	const string SpritesFolderName = "sprites";
+
+	//Called anytime a texture is imported and preprocessed
+	void OnPostprocessTexture(Texture2D texture)
+	{
+		string lowerCaseAssetPath = assetPath.ToLower();
+		bool isInSpritesDirectory = lowerCaseAssetPath.IndexOf("/"+ SpritesFolderName + "/") != -1;
+
+		if(isInSpritesDirectory)
+		{
+			TextureImporter textureImporter = (TextureImporter) assetImporter;
+			textureImporter.textureType = TextureImporterType.Sprite;
+		}
+	}
+}
